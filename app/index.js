620acbdec98d01f6b6425516862afea92f2863a4
Lucas Oliveira <lucas.oliveira@simbioseventures.com>
2023-02-14 18:18:10 -0300

    chore: change expo entry point

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { registerRootComponent } from "expo";
+import { Text, View } from "react-native";
+
+function App() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+export default registerRootComponent(App);
