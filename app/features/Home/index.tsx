2bf50cfaeeae6b5937016bd83db00c1a5e123ef5
Lucas Oliveira <lucas.oliveira@simbioseventures.com>
2023-02-15 23:11:31 -0300

    feat: add home component

--- app/features/Home/index.tsx
+++ app/features/Home/index.tsx
@@ -1,9 +1,22 @@
-import { View, Text } from "react-native";
+import React from "react";
+import { HomeHeader } from "./components/HomeHeader";
+import { SafeAreaView, ScrollView, View } from "react-native";
+import { Funds } from "./components/Funds";
+import { CreditsBanner } from "./components/CreditsBanner";
+import { InfoBanners } from "./components/InfoBanners";
 
 export const HomeScreen: React.FC = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
+    <View style={{ flex: 1 }}>
+      <SafeAreaView>
+        <HomeHeader />
+      </SafeAreaView>
+
+      <ScrollView bounces={false} contentContainerStyle={{ flexGrow: 1 }}>
+        <Funds />
+        <CreditsBanner />
+        <InfoBanners />
+      </ScrollView>
     </View>
   );
 };
